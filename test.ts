58c41305730ae67dbf6fbe8012d251bcbc24b9fe
Arnor Ameti <arnorameti.ch@gmail.com>
2019-03-30 21:23:36 +0100

    heroku prequisites

--- test.ts
+++ test.ts
@@ -1,3 +1,14 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/M152-Projekt-Frontend'));
+
 app.get('*', function(req, res) {
-  res.sendFile(__dirname + "/dist/M152-Projekt-Frontend/index.html");
+  res.sendFile(__dirname + '/dist/M152-Projekt-Frontend/index.html');
 });
+
+// default Heroku port
+app.listen(process.env.PORT || 5000);
+
